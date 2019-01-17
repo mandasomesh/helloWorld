ffe0acd22add510629d5809c7daea5a8f97f8e6d
Somesh.Manda <Somesh.Manda@PL2W3020158.internal.imsglobal.com>
2019-01-17 11:23:39 -0500

    second commit

--- maven-demo/src/main/java/Main.java
+++ maven-demo/src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
 	public static void main (String[] args) 
 	{
 		System.out.println("testing");
-		System.out.println("testing more");
+		System.out.println("testing more for git testing");
 	}
 }
