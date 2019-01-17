aea4b52732b70cf4d1d9e2014ac782d33482d0f9
Somesh.Manda <someshmnda@gmail.com>
2019-01-16 19:18:27 -0500

    initial commit of the project

---
+++ maven-demo/src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+	public static void main (String[] args) 
+	{
+		System.out.println("testing");
+		System.out.println("testing more");
+	}
+}
---
+++ maven-demo/src/test/java/com/codebind/appTest.java
@@ -0,0 +1,15 @@
+package com.codebind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class appTest {
+
+	@Test
+	public void test() {
+		
+	}
+	//dependency-purge-local-repository
+}
+
